25004acfe1ff9a5e3f25d9a112d71129f54ba88c
Keydouu <63008369+Keydouu@users.noreply.github.com>
2021-04-17 15:42:01 +0000

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()
 	graph Automata;
 	int i=0;
 	char input[TAILLE_MAX_DU_INPUT],yesOrNo=0;
+	FILE* fichier=NULL;
 	while((yesOrNo!='y')&&(yesOrNo!='Y')&&(yesOrNo!='n')&&(yesOrNo!='N'))
 	{
 		printf("Voulez vous entrer manuellement l'automate ? [Y/N]\n");
@@ -17,7 +18,29 @@ int main()
 	if((yesOrNo=='Y')||(yesOrNo=='y'))
 		Automata=lectureManuelle();
 	else
-		Automata=lectureDuFichier();
+	{
+		yesOrNo=0;
+		while((yesOrNo!='y')&&(yesOrNo!='Y')&&(yesOrNo!='n')&&(yesOrNo!='N'))
+		{
+			printf("Voulez vous utiliser la derniere automate que vous avez entre ? [Y/N]\n");
+			scanf("%c",&yesOrNo);
+			viderLeBuffer(yesOrNo);
+			system("clear");
+		}
+		if((yesOrNo=='Y')||(yesOrNo=='y'))
+			fichier=fopen("derniereAutomateUtilisee.txt","r");
+		else
+		{
+			printf("Le programme va utiliser l'automate qui se trouve au fichier \"graphe.txt\"\n");
+			fichier=fopen("graphe.txt","r");
+		}
+		if (fichier==NULL)
+		{
+			exit(0);
+		}
+		Automata=lectureDuFichier(fichier);
+		fclose(fichier);
+	}
 	for(i=0;i<TAILLE_MAX_DU_INPUT;i++)
 	input[i]=0;
 	printf("Veuillez entrer le mot à lire\n");
